27f176d5e5068cedce5d17701b58cb9c559c9f99
panfei <1147732951@qq.com>
2020-03-04 13:40:41 +0800

    fix(single_linked_list) recover

--- 02-linkedlist/SingleLinkedList_test.go
+++ 02-linkedlist/SingleLinkedList_test.go
@@ -1,47 +1,49 @@
 package main
 
-///**
-//单链表操作单元测试
-// */
-//func TestLinkedList_InsertToHead(t *testing.T) {
-//	l := NewLinkedList()
-//	for i := 0; i < 10; i++ {
-//		l.InsertToHead(i + 1)
-//	}
-//	l.Print()
-//}
-//
-//func TestLinkedList_InsertToTail(t *testing.T) {
-//	l := NewLinkedList()
-//	for i := 0; i < 10; i++ {
-//		l.InsertToHead(i + 1)
-//	}
-//	l.Print()
-//}
-//
-//func TestLinkedList_FindByIndex(t *testing.T) {
-//	l := NewLinkedList()
-//	for i := 0; i < 10; i++ {
-//		l.InsertToTail(i + 1)
-//	}
-//	t.Log(l.FindByIndex(0))
-//	t.Log(l.FindByIndex(9))
-//	t.Log(l.FindByIndex(5))
-//	t.Log(l.FindByIndex(11))
-//}
-//
-//func TestLinkedList_DeleteNode(t *testing.T) {
-//	l := NewLinkedList()
-//	for i := 0; i < 3; i++ {
-//		l.InsertToTail(i + 1)
-//	}
-//	l.Print()
-//
-//	t.Log(l.DeleteNode(l.Head.Next))
-//	l.Print()
-//
-//	t.Log(l.DeleteNode(l.Head.Next.Next))
-//	l.Print()
-//}
+import "testing"
+
+/**
+单链表操作单元测试
+*/
+func TestLinkedList_InsertToHead(t *testing.T) {
+	l := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		l.InsertToHead(i + 1)
+	}
+	l.Print()
+}
+
+func TestLinkedList_InsertToTail(t *testing.T) {
+	l := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		l.InsertToHead(i + 1)
+	}
+	l.Print()
+}
+
+func TestLinkedList_FindByIndex(t *testing.T) {
+	l := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		l.InsertToTail(i + 1)
+	}
+	t.Log(l.FindByIndex(0))
+	t.Log(l.FindByIndex(9))
+	t.Log(l.FindByIndex(5))
+	t.Log(l.FindByIndex(11))
+}
+
+func TestLinkedList_DeleteNode(t *testing.T) {
+	l := NewLinkedList()
+	for i := 0; i < 3; i++ {
+		l.InsertToTail(i + 1)
+	}
+	l.Print()
+
+	t.Log(l.DeleteNode(l.Head.Next))
+	l.Print()
+
+	t.Log(l.DeleteNode(l.Head.Next.Next))
+	l.Print()
+}
 
 
