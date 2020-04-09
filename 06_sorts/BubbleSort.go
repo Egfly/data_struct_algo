05ea07bf53b96bd775b58ac4f89d664173428014
panfei <1147732951@qq.com>
2020-04-09 16:22:46 +0800

    feat(sorts) add bubble_sort

--- 06_sorts/BubbleSort.go
+++ 06_sorts/BubbleSort.go
@@ -1,6 +1,29 @@
 package main
 
+import "fmt"
+
 //冒泡排序
 func BubbleSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
 
+	for i := 0; i < n; i++ {
+		isChange := false
+		for j := 0; j < n-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+				isChange = true
+			}
+		}
+		if !isChange {
+			break
+		}
+	}
+}
+
+func main()  {
+	a := []int{6,5,4,3,2,1}
+	BubbleSort(a, 6)
+	fmt.Println(a)
 }
