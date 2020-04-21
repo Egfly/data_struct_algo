47342b7a600b104d1e49dadab446fc3f6685539e
panfei <1147732951@qq.com>
2020-04-21 14:09:25 +0800

    feat(sorts) add insertion_sort

--- 06_sorts/BubbleSort.go
+++ 06_sorts/BubbleSort.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //冒泡排序
 func BubbleSort(a []int, n int) {
 	if n <= 1 {
@@ -20,10 +18,4 @@ func BubbleSort(a []int, n int) {
 			break
 		}
 	}
-}
-
-func main()  {
-	a := []int{6,5,4,3,2,1}
-	BubbleSort(a, 6)
-	fmt.Println(a)
 }
--- 06_sorts/InsertionSort.go
+++ 06_sorts/InsertionSort.go
@@ -1,20 +1,30 @@
 package main
 
+import "fmt"
+
 //插入排序
 func InsertionSort(a []int, n int) {
 	if n <= 1 {
 		return
 	}
-	for i := 1; i < n; i++ {
-		value := a[i]
+
+	for i := 0; i < n; i++ {
+		tmp := a[i]
 		j := i - 1
 		for ; j >= 0; j-- {
-			if a[j] > value {
+			if a[j] > tmp {
 				a[j+1] = a[j]
 			} else {
 				break
 			}
 		}
-		a[j+1] = value
+		a[j+1] = tmp
 	}
 }
+
+func main()  {
+	a := []int{6,5,4,3,2,1}
+	InsertionSort(a, 6)
+	fmt.Println(a)
+}
+
